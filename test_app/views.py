d25904cb676040796eec682e7772c4057b783877
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-07-14 00:21:20 +0530

    Add Todo view

--- test_app/views.py
+++ test_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import TodoItem
 
 # Create your views here.
 def home(request):
     return render(request, "index.html")
+
+def todos(request):
+    todo_items = TodoItem.objects.all()
+    return render(request, "todo.html", { "todos": todo_items })
