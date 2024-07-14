79220f70f41c074740ca4a7edb9300b3257fd27e
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-07-14 23:34:26 +0530

    Add router guard

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("register", views.register_view, name="register-page"),
     path("login", auth_views.LoginView.as_view(template_name="users/login.html"), name="login-page"),
-    path("logout", views.logout_view, name="logout-page")
+    path("logout", views.logout_view, name="logout-page"),
+    path("profile", views.profile_view, name="profile-page")
 ]
