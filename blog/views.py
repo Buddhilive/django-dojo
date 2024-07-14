8e7e82e01b80f57f700e18a9e570c3e9b79f7c04
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-07-14 20:59:15 +0530

    Fetch data from DB

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,8 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Buddhi Kavindra',
-        'title': 'Welcome',
-        'content': 'Lorem ipsum...',
-        'date_posted': 'May 13, 2024'
-    },
-    {
-        'author': 'John Doe',
-        'title': 'Getting Started',
-        'content': 'Lorem ipsum...',
-        'date_posted': 'June 13, 2024'
-    }
-]
+from .models import BlogPosts
 
 def view_home(request):
     context = {
-        'blog_posts': posts
+        'blog_posts': BlogPosts.objects.all()
     }
     return render(request, "blog/index.html", context)
