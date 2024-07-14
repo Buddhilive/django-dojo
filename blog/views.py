3dcad6f571d584a87febc03f85f0c60bc2fef75a
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-07-14 15:52:26 +0530

    Load dummy data

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts = [
+    {
+        'author': 'Buddhi Kavindra',
+        'title': 'Welcome',
+        'content': 'Lorem ipsum...',
+        'date_posted': 'May 13, 2024'
+    },
+    {
+        'author': 'John Doe',
+        'title': 'Getting Started',
+        'content': 'Lorem ipsum...',
+        'date_posted': 'June 13, 2024'
+    }
+]
+
 def view_home(request):
-    return render(request, "blog/index.html")
+    context = {
+        'blog_posts': posts
+    }
+    return render(request, "blog/index.html", context)
