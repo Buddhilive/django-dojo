e9f7b323b97fef7d9533df1a884a0fd5e3d00bfe
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-07-14 15:34:54 +0530

    Add templates

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def view_home(request):
-    return HttpResponse("<h1>Welcome to Blog<h2>")
+    return render(request, "blog/index.html")
