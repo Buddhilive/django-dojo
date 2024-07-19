5cd1329622e5eaa4c7e5a9bad9bb00000d3a00cd
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-07-19 07:16:54 +0530

    Update blog list view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import BlogPost
 
 def view_home(request):
@@ -6,3 +7,10 @@ def view_home(request):
         'blog_posts': BlogPost.objects.all()
     }
     return render(request, "blog/index.html", context)
+
+class PostListView(ListView):
+    model = BlogPost
+    # <app>/<model>_<viewtype>.html
+    template_name = 'blog/index.html'
+    context_object_name = 'blog_posts'
+    ordering = ['-date_posted']
