f6223ea709e63a9a7fd0e604d4a2beebcadd4836
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-07-19 08:03:06 +0530

    Post single view

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
-    path("", PostListView.as_view(), name="blog-home")
+    path("", PostListView.as_view(), name="blog-home"),
+    path("blog/<int:pk>/", PostDetailView.as_view(), name="blog-post-view")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import BlogPost
 
 def view_home(request):
@@ -14,3 +14,6 @@ class PostListView(ListView):
     template_name = 'blog/index.html'
     context_object_name = 'blog_posts'
     ordering = ['-date_posted']
+    
+class PostDetailView(DetailView):
+    model = BlogPost
