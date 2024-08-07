77df0bc368ae15f397b09b533bfa6746807d5ebb
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-08-07 22:00:00 +0530

    Add pagintor

--- blog/views.py
+++ blog/views.py
@@ -21,6 +21,7 @@ class PostListView(ListView):
     template_name = 'blog/index.html'
     context_object_name = 'blog_posts'
     ordering = ['-date_posted']
+    paginate_by = 1
     
 class PostDetailView(DetailView):
     model = BlogPost
